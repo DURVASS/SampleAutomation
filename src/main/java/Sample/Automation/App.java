a5d88a7051af4676bfed7b4c701469c083f16127
sandeep <sundeepdurvasula@gmsil.com>
2019-08-02 14:04:02 +0530

    start webdriver

--- src/main/java/Sample/Automation/App.java
+++ src/main/java/Sample/Automation/App.java
@@ -1,13 +1,16 @@
 package Sample.Automation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    public static void main(String[] args) {
+		WebDriver driver =new FirefoxDriver();
+
+	}
 }
